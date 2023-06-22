59586c9ab0c211286e8947cf6290e0a8347f6bd9
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-06-22 11:00:34 -0700

    Opcode functions

---
+++ opcode_functions.c
@@ -0,0 +1,118 @@
+#include "monty.h"
+
+/**
+ * fadd - Adds the top two elements of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number used to call add function.
+ */
+
+void fadd(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		args.found_error = EXIT_FAILURE;
+		return;
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
+
+/**
+ * fpop - Removes the top element of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number used to call the pop function.
+ */
+
+void fpop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		args.found_error = EXIT_FAILURE;
+		return;
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
+}
+
+/**
+ * fswap - Swaps the top two elements of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number used to call the swap function.
+ */
+
+
+void fswap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		args.found_error = EXIT_FAILURE;
+		return;
+	}
+
+	temp = (*stack)->next;
+	(*stack)->prev = temp;
+	(*stack)->next = temp->next;
+
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+
+	temp->prev = NULL;
+	temp->next = *stack;
+	*stack = temp;
+
+}
+
+/**
+ * fpint - Prints the value the top of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number used to call pint function.
+ */
+
+void fpint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		args.found_error = EXIT_FAILURE;
+		return;
+	}
+
+	fprintf(stdout, "%d\n", (*stack)->n);
+}
+
+/**
+ * fpall- Prints all the elements of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number used to call pall function.
+ */
+
+void fpall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	(void)line_number;
+
+	while (temp != NULL)
+	{
+		fprintf(stdout, "%d\n", temp->n);
+		temp = temp->next;
+	}
+}
---
+++ opcode_functions2.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * fpush - Pushes an integer on the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number used to call push function.
+ */
+
+void fpush(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+	int value_node;
+
+	if (!args.value)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		args.found_error = EXIT_FAILURE;
+		return;
+	}
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: Malloc failed\n");
+		args.found_error = EXIT_FAILURE;
+		return;
+	}
+
+	value_node = atoi(args.value);
+	new_node->n = value_node;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+}
