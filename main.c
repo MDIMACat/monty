b748055dfcd28f9918d400878ff4900e5ca16ae9
MDIMACat <romanmdima@gmail.com>
2023-06-22 19:35:40 +0200

    README.md, Monty.h, Main.c

---
+++ main.c
@@ -0,0 +1,60 @@
+#include "monty.h"
+
+/**
+ * tokenize_line-tokenizes the line
+ * @str: string parameter
+ * Return: tokenized line
+*/
+
+char **tokenize_line(char *str);
+arg_t args = {NULL, NULL, NULL, NULL, 0, 0, 0};
+
+/**
+ * main - main function
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ * Return: 1 of fail and 0 in success
+ */
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
+	char line[100];
+	size_t len = 100;
+	char *read;
+	unsigned int line_number = 0;
+	size_t line_length;
+
+	check_command_line_arguments(argc, argv[1]);
+	get_stream(argv[1]);
+	while ((read = fgets(line, len, args.file)) != NULL)
+	{
+		if (args.found_error == 1)
+			break;
+		line_number++;
+		line_length = strlen(line);
+		if (line_length > 0 && line[line_length - 1] == '\n')
+			line[line_length - 1] = '\0';
+		args.line_number = line_number;
+		args.tokens = tokenize_line(line);
+		execute_opcode(&stack, line_number);
+		free(args.tokens);
+	}
+	fclose(args.file);
+	free_stack(stack);
+	exit(args.found_error);
+}
+
+/**
+ * get_stream - opens file
+ * @filename: filename parameter
+ * Return: Void
+ */
+
+void get_stream(const char *filename)
+{
+	args.file = fopen(filename, "r");
+	if (args.file == NULL)
+	{
+		handle_file_open_error(filename);
+	}
+}
---
+++ monty.h
@@ -0,0 +1,75 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct arg_s - global variable struct
+ * @file: pointer member
+ * @tokens: pointer member
+ * @value: pointer member
+ * @line: pointer member
+ * @line_number: line number member
+ * @tokens_count: tokens count member
+ * @found_error: find error member
+ *
+ * Description: Struct defined to at as a global variable
+*/
+typedef struct arg_s
+{
+	FILE *file;
+	char **tokens;
+	char *value;
+	char *line;
+	unsigned int line_number;
+	int tokens_count;
+	int found_error;
+} arg_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Function prototypes*/
+void get_stream(const char *filename);
+void execute_opcode(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+void handle_file_open_error(const char *filename);
+void check_command_line_arguments(int argc, char *filename);
+void fpush(stack_t **stack, unsigned int line_number);
+void fpall(stack_t **stack, unsigned int line_number);
+void fpint(stack_t **stack, unsigned int line_number);
+void fswap(stack_t **stack, unsigned int line_number);
+void fpop(stack_t **stack, unsigned int line_number);
+void fadd(stack_t **stack, unsigned int line_number);
+extern arg_t args;
+
+#endif
